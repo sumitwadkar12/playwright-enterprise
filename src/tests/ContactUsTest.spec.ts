6126fb3bcf584b3d1962573a20131e5665088937
sumitwadkar12 <sumitwadkar12@gmail.com>
2026-01-01 19:51:58 +0530

    sss

--- src/tests/ContactUsTest.spec.ts
+++ src/tests/ContactUsTest.spec.ts
@@ -13,5 +13,6 @@ for (const data of testData) {
         await home.enterContactUsDetails(data.Category, data.Product, data.Email, data.Subject);
         await home.sendMessage();
         await home.verifySuccessMessage(data.Message);
+
     });
 }
